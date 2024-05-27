8f6874a1f74fce914b9c9c9a95dc12d7d49ef53d
abdullahkassem <abdullahkassem@aucegypt.edu>
2024-05-28 02:58:06 +0300

    Made style responsive for tablets size

--- my-post-app/public/HamburgerMenu.jsx
+++ my-post-app/public/HamburgerMenu.jsx
@@ -3,8 +3,7 @@ function Icon(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="800"
-      height="800"
+      
       fill="none"
       viewBox="0 0 24 24"
       {...props}
--- my-post-app/src/components/Header.jsx
+++ my-post-app/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import PageTitle from "./PageTitle";
 import UserPP from "./UserPP";
 import HamburgerMenu from "../../public/HamburgerMenu"
+
 export default function Header({title}) {
   return (
     <header>
         <UserPP/>
         <PageTitle title={title}/>
-        <HamburgerMenu width= "30" height="30"/>
+        <HamburgerMenu />
     </header>
   )
 }
